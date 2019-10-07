ed7ce3dcea67adecbfe21f21aba84e21c3e19fc6
MagnusLj <magnusljungvall@hotmail.com>
2019-10-07 17:29:30 +0100

    Working with name stuff locally

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -2,15 +2,15 @@
     <main>
         <Nav />
         <div class="wrapper">
-        <br><h2><br>Chat</h2>
+        <br><h2><br>Chattrum</h2>
 
 
   <div class="card mt-3">
       <div class="card-body">
-          <div class="card-title">
-              <h3>Chat Group</h3>
+          <!-- <div class="card-title">
+              <h3>Chattrummet</h3>
               <hr>
-          </div>
+          </div> -->
           <div class="card-body_2">
               <div class="messages" v-for="(msg, index) in messages" :key="index">
                   <p><span class="font-weight-bold">{{ msg.user }}: </span>{{ msg.message }}</p>
@@ -18,20 +18,59 @@
           </div>
       </div>
       <div class="card-footer">
+          <p v-if="!this.user2">Du måste välja ett chattnamn för att kunna chatta</p>
+          <p v-else>Chatta så det ryker, {{ user2 }}!</p>
           <form @submit.prevent="sendMessage">
               <div class="form-group">
-                  <label for="user">User:</label>
-                  <input type="text" v-model="user" class="form-control">
+                  <label for="user2">Användare:</label>
+                  <input type="text" v-model="user2" class="form-control">
               </div>
               <div class="form-group pb-3">
-                  <label for="message">Message:</label>
+                  <label for="message">Meddelande:</label>
                   <input type="text" v-model="message" class="form-control">
               </div>
-              <button type="submit" class="btn btn-success">Send</button>
+              <button type="submit" v-if="!this.user2" class="btn btn-success" disabled>Skicka</button> <button type="submit" v-else class="btn btn-success">Skicka</button> <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Välj namn</button>
           </form>
       </div>
   </div>
   </div>
+
+  <!-- Trigger the modal with a button -->
+  <!-- <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button> -->
+
+  <!-- Modal -->
+  <div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <!-- <h4 class="modal-title">Modal Header</h4> -->
+        </div>
+        <div class="modal-body">
+          <!-- <p>Some text in the modal.</p> -->
+          <label for="user">Chattnamn&nbsp;&nbsp;</label>
+          <input
+            id="user"
+            v-model="user"
+            type="text"
+            name="user"
+          >
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal" @click="userToUser2">Spara</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+
+
+
+
   </main>
 </template>
 
@@ -49,6 +88,7 @@ export default {
     data() {
         return {
             user: '',
+            user2: '',
             message: '',
             messages: [],
             socket : io('localhost:3000')
@@ -63,13 +103,24 @@ export default {
                 message: this.message
             });
             this.message = ''
+        },
+
+        openModal() {
+                this.toggleModal = true;
+    },
+
+        userToUser2() {
+            this.user2 = this.user;
         }
+
+
     },
     mounted() {
         this.socket.on('MESSAGE', (data) => {
             this.messages = [...this.messages, data];
             // you can also do this.messages.push(data)
         });
+        this.openModal();
     }
 }
 </script>
@@ -85,6 +136,10 @@ h2 {
 .card-body_2 {
     height: 10em;
     overflow-y:scroll;
+}
+
+.btn-info {
+    float: right
 }
 
 </style> -->
